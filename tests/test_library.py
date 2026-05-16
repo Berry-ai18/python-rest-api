4cbb367d829505535cb58049d71ab3eadc15e95e
Patrik <57407450+Berry-ai18@users.noreply.github.com>
2026-05-16 14:24:58 +0200

    ADDING MORE TESTS

--- tests/test_library.py
+++ tests/test_library.py
@@ -37,11 +37,15 @@ def create_books(base_url):
     for book_id in ids:
         requests.delete(base_url + f"books/{book_id}")
 
+
+# GET ALL BOOKS 
 def test_get_all_books(base_url, create_books):
     response = requests.get(base_url + 'books')
     assert response.status_code == 200
     assert len(response.json()) >= 3
 
+
+# VALIDATE THAT THERE ARE CORRECT PROPERTIES IN RESPONSE
 def test_validate_all_books(base_url):
     response = requests.get(base_url + 'books')
     
@@ -64,13 +68,69 @@ def test_validate_all_books(base_url):
         assert isinstance(book['rating'], float)
     
 
-
+# USING GET METHOD TO GET SPECIFIC BOOK
 def test_get_specific_book(base_url, create_book):
 
     response = requests.get(base_url + f'books/{create_book}')
 
     assert response.status_code == 200
 
+def test_get_invalid_book(base_url, create_books):
+    response = requests.get(base_url + 'books/9999')
+    assert response.status_code == 404
+
+def test_post_book(base_url):
+    payload = {
+        'title':'Crazy',
+        'author': 'Pato',
+        'genre':'drama',
+        'rating': '3.5'
+    }
+
+    response = requests.post(base_url + 'books', json = payload)
+    assert response.status_code == 201
+    data = response.json()
+    assert 'Crazy' in data['title']
 
 
+def test_post_book_notitle(base_url):
+    payload = {
+        'author': 'Pato',
+        'genre':'drama',
+        'rating': '3.5'
+    }
+
+    response = requests.post(base_url + 'books', json = payload)
+    assert response.status_code == 500
+
+def test_post_book_incorrect_rating(base_url):
+    payload = {
+        'title':'Crazy',
+        'author': 'Pato',
+        'genre':'drama',
+        'rating': 'STRING INSTEAD OF FLOAT'
+    }
+
+    response = requests.post(base_url + 'books', json = payload)
+    assert response.status_code == 500
+
+
+def test_put_book(base_url, create_book):
+    payload = {
+        'title':'Crazy',
+        'author': 'Pato',
+        'genre':'drama',
+        'rating': '3.4'
+    }
+
+    response = requests.put(base_url + f'books/{create_book}', json = payload)
+    assert response.status_code == 200
+
+
+def test_patch_method(base_url, create_book):
+    response = requests.patch(base_url + f"books/{create_book}", json = {"title": "Hangover"})
+    assert response.status_code == 200
+    data = response.json()
+    assert 'title' in data
+    assert 'Hangover' in data['title']
 
